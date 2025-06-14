cebcc4be18033b17efbeb67498bc44324b68e5f5
mdyeasinhn <mdyeasinhn@gmail.com>
2025-06-15 00:39:48 +0600

    Sorting Objects

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//1.Task: Array Filtering and Mapping
+// 1. Array Filtering and Mapping
 const people = [
     { name: "enan", age: 25, gender: "female" },
     { name: "yeasin", age: 30, gender: "male" },
@@ -11,7 +11,7 @@ const filterMalesAndMapNames = (arr) =>
 //onsole.log(filterMalesAndMapNames(people));
 
 
-//2.Object manipulation
+// 2.Object manipulation
 const books = [
     { title: "Book", author: "yeasin", year: 2002 },
     { title: "The Book", author: "enan", year: 2024 },
@@ -22,7 +22,7 @@ const getBookTitles = (arr) => arr.map(book => book.title);
 //console.log(getBookTitles(books)); 
 
 
-//3-->Function Composition
+// 3-->Function Composition
 
 const square = n => n * n;
 const double = n => n * 2;
@@ -31,3 +31,14 @@ const addFive = n => n + 5;
 const composedFunction = n => addFive(double(square(n)));
 
 //console.log(composedFunction(3)); 
+
+// 4.Sorting Objects
+const cars = [
+  { make: "Toyota", model: "Camry", year: 2018 },
+  { make: "Honda", model: "Civic", year: 2015 },
+  { make: "Ford", model: "Focus", year: 2020 },
+];
+
+const sortByYear = (arr) => arr.sort((a, b) => a.year - b.year);
+
+//console.log(sortByYear(cars));
