0a9dd847dcebabb23baf6e5c273529e9584543f7
mdyeasinhn <mdyeasinhn@gmail.com>
2025-06-15 00:30:25 +0600

    Object manipulation

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 //1.Task: Array Filtering and Mapping
 const people = [
-  { name: "enan", age: 25, gender: "female" },
-  { name: "yeasin", age: 30, gender: "male" },
-  { name: "arafat", age: 22, gender: "male" },
+    { name: "enan", age: 25, gender: "female" },
+    { name: "yeasin", age: 30, gender: "male" },
+    { name: "arafat", age: 22, gender: "male" },
 ];
 
 const filterMalesAndMapNames = (arr) =>
-  arr.filter(p => p.gender === "male").map(p => p.name);
+    arr.filter(p => p.gender === "male").map(p => p.name);
 
 console.log(filterMalesAndMapNames(people));
+
+
+//Object manipulation
+const books = [
+    { title: "Book", author: "yeasin", year: 2002 },
+    { title: "The Book", author: "enan", year: 2024 },
+];
+
+const getBookTitles = (arr) => arr.map(book => book.title);
+
+console.log(getBookTitles(books)); 
